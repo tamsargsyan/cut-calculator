ffadd39f2004b2a8be5dd62b294a99d0467230f1
Tam <tam@Tams-MacBook-Pro.local>
2023-08-30 03:45:09 -0700

    install react layout

---
+++ src/components/ShowcaseLayout/index.js
@@ -0,0 +1,109 @@
+import React, { useState, useEffect } from "react";
+import PropTypes from "prop-types";
+import _ from "lodash";
+import { Responsive, WidthProvider } from "react-grid-layout";
+import "./style.css"
+const ResponsiveReactGridLayout = WidthProvider(Responsive);
+
+// Define generateLayout function
+const generateLayout = () => {
+  return _.map(_.range(0, 25), (item, i) => {
+    const y = Math.ceil(Math.random() * 4) + 1;
+    return {
+      x: (_.random(0, 5) * 2) % 12,
+      y: Math.floor(i / 6) * y,
+      w: 2,
+      h: y,
+      i: i.toString(),
+      static: false,
+    };
+  });
+};
+
+const ShowcaseLayout = (props) => {
+  const [currentBreakpoint, setCurrentBreakpoint] = useState("lg");
+  const [compactType, setCompactType] = useState("vertical");
+  const [mounted, setMounted] = useState(false);
+  const [layouts, setLayouts] = useState({ lg: props.initialLayout });
+
+  useEffect(() => {
+    setMounted(true);
+  }, []);
+
+  const generateDOM = () => {
+    return _.map(layouts.lg, (l, i) => (
+      <div key={i} className={l.static ? "static" : ""}>
+        {l.static ? (
+          <span
+            className="text"
+            title="This item is static and cannot be removed or resized."
+          >
+            Static - {i}
+          </span>
+        ) : (
+          <span className="text">{l.i}</span>
+        )}
+      </div>
+    ));
+  };
+  console.log(layouts)
+  const onBreakpointChange = (breakpoint) => {
+    setCurrentBreakpoint(breakpoint);
+  };
+
+  const onCompactTypeChange = () => {
+    const newCompactType =
+      compactType === "horizontal"
+        ? "vertical"
+        : compactType === "vertical"
+        ? null
+        : "horizontal";
+    setCompactType(newCompactType);
+  };
+
+  const onLayoutChange = (layout, layouts) => {
+    props.onLayoutChange(layout, layouts);
+  };
+
+  const onNewLayout = () => {
+    setLayouts({ lg: generateLayout() });
+  };
+
+  return (
+    <div>
+      <div>
+        Current Breakpoint: {currentBreakpoint} ({props.cols[currentBreakpoint]}{" "}
+        columns)
+      </div>
+      <div>Compaction type: {_.capitalize(compactType) || "No Compaction"}</div>
+      <button onClick={onNewLayout}>Generate New Layout</button>
+      <button onClick={onCompactTypeChange}>Change Compaction Type</button>
+      <ResponsiveReactGridLayout
+        {...props}
+        layouts={layouts}
+        onBreakpointChange={onBreakpointChange}
+        onLayoutChange={onLayoutChange}
+        measureBeforeMount={false}
+        useCSSTransforms={mounted}
+        compactType={compactType}
+        preventCollision={!compactType}
+      >
+        {generateDOM()}
+      </ResponsiveReactGridLayout>
+    </div>
+  );
+};
+
+ShowcaseLayout.propTypes = {
+  onLayoutChange: PropTypes.func.isRequired,
+};
+
+ShowcaseLayout.defaultProps = {
+  className: "layout",
+  rowHeight: 30,
+  onLayoutChange: () => {},
+  cols: { lg: 12, md: 10, sm: 6, xs: 4, xxs: 2 },
+  initialLayout: generateLayout(),
+};
+
+export default ShowcaseLayout;
